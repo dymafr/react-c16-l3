cecf057805949c9f57e01439306a66893b1e9e6d
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-05 18:26:57 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styles from './App.module.scss';
-import { useState, lazy, Suspense } from 'react';
-import ComposantA from './pages/ComposantA/ComposantA';
+import { useState, lazy, Suspense, useTransition } from 'react';
+
+const ComposantA = lazy(
+  () =>
+    new Promise((res) =>
+      setTimeout(() => res(import('./pages/ComposantA/ComposantA')), 3000)
+    )
+);
 
 const ComposantB = lazy(
   () =>
@@ -12,6 +18,7 @@ const ComposantB = lazy(
 
 function App() {
   const [page, setPage] = useState('a');
+  const [isPending, startTransition] = useTransition('a');
 
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
@@ -19,14 +26,26 @@ function App() {
         <button className="btn btn-primary mr-5" onClick={() => setPage('a')}>
           Composant A
         </button>
-        <button className="btn btn-primary mr-5" onClick={() => setPage('b')}>
+        <button
+          className="btn btn-primary mr-5"
+          onClick={() => startTransition(() => setPage('b'))}
+        >
           Composant B
         </button>
       </nav>
       <div className="flex-fill p-20">
         <Suspense fallback={<h3>Chargement...</h3>}>
           {page === 'a' && <ComposantA />}
-          {page === 'b' && <ComposantB />}
+          <Suspense fallback={<h3>Chargement...</h3>}>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+              eveniet ea id quasi aperiam repellat aut, soluta tenetur
+              voluptatibus ducimus odio tempore nostrum labore dolorem maiores
+              sapiente optio ut eos.
+            </p>
+            {isPending && <small>Petit chargement...</small>}
+            {page === 'b' && <ComposantB />}
+          </Suspense>
         </Suspense>
       </div>
     </div>
